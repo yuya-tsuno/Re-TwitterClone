9e50c34649805a37f97890ca1fa7ed3980781777
Tsuno Yuya <2ch.yuya@gmail.com>
2019-12-09 21:42:34 +0900

    フロント部分

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,6 +1,6 @@
 module TweetsHelper
     def choose_new_or_edit
-        if action_name == 'new' || action_name == 'confirm' ||　action_name == 'create'
+        if action_name == 'new' || action_name == 'confirm' || action_name == 'create'
           confirm_tweets_path
         elsif action_name == 'edit'
           tweet_path
